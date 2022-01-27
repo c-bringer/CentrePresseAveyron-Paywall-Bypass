51709906deb76ae8ea4f448d6d45b690a4279121
Kermit <44336706+c-bringer@users.noreply.github.com>
2022-01-27 16:12:04 +0100

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1,3 +1,4 @@
+//1.0
 var e = null,
     t = document.querySelector('[rel="js-article-premium"]');
 
@@ -8,3 +9,14 @@ var a = t.dataset.articleToken;
             $(".paywall").hide()
     }
 )))
+
+//2.0
+var e = null,
+    t = document.querySelector('[rel="js-article-premium"]');
+
+var a = t.dataset.articleToken;
+!a || e && e !== a || (this.serverPaywall ? $.get("/ajax/article-content?k=" + a, (function(e) {
+    $("[rel=js-article-content]").html(e),
+    $(".paywall").hide()
+})) : ($("[rel=js-article-content]").removeAttr("data-state").removeAttr("data-selection").removeAttr("style"),
+$(".paywall").hide()))
